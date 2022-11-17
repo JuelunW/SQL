d54e042808a55ad648e9ceda92e7e04c23463f4c
JuelunW <117949546+JuelunW@users.noreply.github.com>
2022-11-17 17:24:36 -0500

    Add files via upload

---
+++ quickTest.sql
@@ -0,0 +1,51 @@
+--Question 1
+SELECT
+    e.firstname || ' ' || e.lastname AS employee,
+    m.firstname || ' ' || m.lastname AS manager
+FROM
+    employee e
+INNER JOIN employee m ON m.employee_id = e.manager_id
+ORDER BY employee;
+
+
+
+--Question 2
+SELECT 
+	id,
+	salary,
+	department
+FROM (SELECT
+	  id,
+	  salary,
+	  department,
+	  DENSE_RANK() OVER( PARTITION BY department ORDER BY salary DESC) AS dr
+	  FROM compensation) AS tmp
+WHERE dr = 1;
+
+
+
+--Question 3
+--Transform from item_sold_a into item_sold_b
+SELECT date_sk, 
+	'a' as item, 
+	item_a as qty 
+FROM items_sold_a
+UNION ALL
+SELECT date_sk, 
+	'b' as item, 
+	item_b as qty 
+FROM items_sold_a
+UNION ALL
+SELECT date_sk, 
+	'c' as item, 
+	item_c as qty 
+FROM items_sold_a
+ORDER BY date_sk, item;
+
+--Transform from item_sold_b into item_sold_a
+SELECT date_sk, 
+	SUM(CASE WHEN item = 'a' THEN qty ELSE 0 END) AS item_a,
+	SUM(CASE WHEN item = 'b' THEN qty ELSE 0 END) AS item_b, 
+	SUM(CASE WHEN item = 'c' THEN qty ELSE 0 END) AS item_c
+FROM items_sold_b
+GROUP BY date_sk;
